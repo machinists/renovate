9699fdfe16a8f66b010708b923aef10ecab70bcc
elliottmb <2555214+elliottmb@users.noreply.github.com>
2021-11-05 21:54:36 -0500

    hum

--- lib/manager/concourse-resources/types.ts
+++ lib/manager/concourse-resources/types.ts
@@ -1,21 +1,21 @@
-export type ConcourseDependency = {
-  name: string;
-  type: string;
-  source: ConcourseDockerImageDependency | ConcourseRegistryImageDependency;
-};
-
 export type ConcourseDockerImageDependency = {
   registry_mirror?: string;
   repository: string;
   tag: string;
 };
 
-// TODO: Support
 export type ConcourseRegistryImageDependency = {
-  // TODO: semver_constraint and variant
   registry_mirror?: string;
   repository: string;
+  semver_constraint?: string;
   tag: string;
+  variant?: string;
+};
+
+export type ConcourseDependency = {
+  name: string;
+  type: string;
+  source: ConcourseDockerImageDependency | ConcourseRegistryImageDependency;
 };
 
 export type ConcoursePipeline = {
